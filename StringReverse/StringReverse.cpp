f18e7be28cbfca2ba869f9212aa7918f855268d5
David Clarke <davidclarke1990@gmail.com>
2020-06-06 08:44:10 +0100

    Adding comments

--- StringReverse/StringReverse.cpp
+++ StringReverse/StringReverse.cpp
@@ -5,14 +5,22 @@
 
 void stringReverse(char* _str)
 {
+    // Work out the length of the string
     int length = strlen(_str);
+    // Start one index at the beginning
     int index = 0;
+    // Start one at the end (the last char before the null terminator)
     int endIndex = length-1;
+
+    // Only go half way since by then, the operation will be complete.
     while (index < length / 2)
     {
+        // Perform the swap without needing a temp
         _str[index] = _str[index] + _str[endIndex];
         _str[endIndex] = _str[index] - _str[endIndex];
         _str[index] = _str[index] - _str[endIndex];
+        
+        // Advance the indices 
         ++index;
         --endIndex;
     }
